1e814da84b96b4a3445f997c75cac40041be254c
devartmaker <40697700+devartmaker@users.noreply.github.com>
2023-08-09 16:58:15 +0900

    Update AstronautPlayer.cs

--- packages/Astronaut/AstronautPlayer.cs
+++ packages/Astronaut/AstronautPlayer.cs
@@ -3,50 +3,54 @@ using UnityEngine;
 namespace AstronautPlayer
 {
 
-    public class AstronautPlayer : MonoBehaviour {
+    public class AstronautPlayer : MonoBehaviour
+    {
 
-		private Animator anim;
-		private CharacterController controller;
+        private Animator anim;
+        private CharacterController controller;
 
-		public float speed = 10.0f;
-		public float turnSpeed = 150.0f;
+        public float speed = 10.0f;
+        public float turnSpeed = 150.0f;
         public float jumpForce = 15f;
-		public float gravity = 30.0f;
+        public float gravity = 30.0f;
         private Vector3 moveDirection = Vector3.zero;
 
-		void Start () {
-			controller = GetComponent<CharacterController>();
-			anim = gameObject.GetComponentInChildren<Animator>();
-		}
+        void Start()
+        {
+            controller = GetComponent<CharacterController>();
+            anim = gameObject.GetComponentInChildren<Animator>();
+        }
 
-		void Update (){
-			if (controller.isGrounded){
-				moveDirection = transform.forward * Input.GetAxisRaw("Vertical") * speed;
+        void Update()
+        {
+            if (controller.isGrounded)
+            {
+                moveDirection = transform.forward * Input.GetAxisRaw("Vertical") * speed;
 
                 if (moveDirection == Vector3.zero)
-				{
+                {
                     anim.SetInteger("AnimationPar", 0);
                 }
-				else
-				{
+                else
+                {
                     anim.SetInteger("AnimationPar", 1);
                 }
-			}
-			else
-			{
+            }
+            else
+            {
                 anim.SetInteger("AnimationPar", 0);
             }
 
             if (controller.isGrounded && Input.GetButton("Jump"))
             {
-				print("Jump");
                 moveDirection.y = jumpForce;
+                anim.SetTrigger("Jump");
             }
 
             float turn = Input.GetAxis("Horizontal");
-			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
-			controller.Move(moveDirection * Time.deltaTime);
-			moveDirection.y -= gravity * Time.deltaTime;
-		}
-	}
+            transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+            controller.Move(moveDirection * Time.deltaTime);
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+    }
 }
